b5eca25229fce530814271be89ee7f80226c31ed
Ann <ann.syharevskaya@gmail.com>
2021-01-31 21:54:08 +0300

    add Navbar and pages for it

---
+++ src/components/Navbar.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <!--  навигация-->
+    <v-app-bar
+      color="deep-purple"
+      dark
+    >
+      <v-app-bar-nav-icon
+        @click="sideNav = !sideNav"
+        class="hidden-md-and-up"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          text
+          v-for="(link,i) in links"
+          :key="i"
+          :to="link.url"
+        >
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="sideNav"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item
+            v-for="(link,i) in links"
+            :key="i"
+            :to="link.url"
+          >
+            <v-list-item-icon>
+              <v-icon>{{link.icon}}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{link.title}}</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <!--  /навигация-->
+
+    <!--  основной контент-->
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </div>
+
+</template>
+<script>
+export default {
+  data() {
+    return {
+      sideNav:false, //по умолчаниювыпадменюскрыто
+      drawer: false,
+      group: null,
+      links:[
+        {title: 'Login', icon:'account_box', url:'/login'},
+        {title: 'Register', icon:'face', url:'/register'},
+        {title: 'Cart', icon:'shopping_cart', url:'/checkout'},
+        {title: 'New product', icon:'add', url:'/new'},
+        {title: 'My products', icon:'list', url:'/list'}
+      ],
+    }
+  },
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,65 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/components/Home'
+import Product from '@/components/Products/Product'
+import ProductList from '@/components/Products/ProductList'
+import NewProduct from '@/components/Products/NewProduct'
+import Checkout from '@/components/User/Checkout'
+import Login from '@/components/Auth/Login'
+import Register from '@/components/Auth/Register'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/product/:id',
+    name: 'product',
+    component: Product
+  },
+  {
+    path: '/list',
+    name: 'list',
+    component: ProductList
+  },
+  {
+    path: '/new',
+    name: 'new',
+    component: NewProduct
+  },
+  {
+    path: '/checkout',
+    name: 'checkout',
+    component: Checkout
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
